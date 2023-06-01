74f5160585df0b035d6b2cb034311a20033cef06
Vicente Junior <vicente19981@live.com>
2023-06-01 21:07:53 +0000

    feat: allowing field type password

--- src/app/login/components/form.tsx
+++ src/app/login/components/form.tsx
@@ -56,6 +56,7 @@ const LoginForm: React.FC = () => {
                         id="password"
                         name="password"
                         placeholder="Password"
+                        type="password"
                         className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
                     />
                     <button
